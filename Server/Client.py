cd6ad233037f4ee612769f76aec652328416d83e
rdanderson521 <rdanderson521@gmail.com>
2019-11-28 17:21:19 +0000

    bot done (hopefully)

--- Server/Client.py
+++ Server/Client.py
@@ -3,30 +3,37 @@ import select
 import errno
 import sys
 import re
+from datetime import datetime
+import time
+import argparse
+
 
 
 class IRCBot(object):
-    
 
-    def __init__(self, ip, user = "Bot", name = "Bot", nick = "Bot", channel = ""):
+
+
+    def __init__(self, ip = "127.0.0.1", user = "Bot", name = "Bot", nick = "Bot", channel = ""):
         self.ip = ip
         self.port = 6667
         self.nickname = nick
         self.name = name
         self.user = user
+        self.channel = channel
         self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.rec_buffer = ""
-        self.line_regex = re.compile("\r?\n")
+        self.line_regex = re.compile(r"\r?\n")
         self.test = True
 
-        
+
 
     def connect(self):
         self.socket.connect((self.ip, self.port))
         self.socket.setblocking(False)
         self.send_msg("USER " + self.user + " " + self.user + " " + self.user + " :" + self.name)
         self.send_msg("NICK " + self.nickname)
-        self.send_msg("JOIN #test")
+        if (self.channel != ""):
+            self.send_msg("JOIN #" + self.channel)
 
     def send_msg(self, msg):
         self.socket.send((msg + "\r\n").encode())
@@ -61,76 +68,99 @@ class IRCBot(object):
                 continue
 
             line_split = {}
-            line_commands = {}
-            line_arguments = {}
-            arguments = {}
-            command = ""
+            command_args = {}
             msg = ""
-            msg_from = ""
+
+            #print(line)
 
             if (line.find(":") != -1):
-                line_split = line.split(":")
+                line_split = line.strip(":").split(":")
 
-                if (line_split[0] == ""):
-                    line_split = line_split[1:]
+                command_args = line_split[0].strip(" ").split(" ")
 
-                    if (len(line_split) > 1):
-                        msg = line_split[-1]
+                if (len(line_split) > 1):
+                    msg = line_split[-1]
 
-                elif (len(line_split) > 1):
-                        msg_from = line_split[-1]
+            #print(str(command_args) + " > " + msg)
 
-                
-
-                if (line_split[0].find(" ") != -1):
-                    line_arguments = line_split[0].split(" ")
-
-                    if (len(line_arguments) > 2):
-                        command = line_arguments[1]
-                        msg_from = line_arguments[0]
-                        if (line_arguments[2] == self.nickname and len(line_arguments) > 3):
-                            arguments = line_arguments[3:]
-                            
-                    else:
-                        command = line_arguments[0]
-
-            print(msg_from + " (" + command + str(arguments) +  ")> " + msg)
-
-            #self.handle_command(command,arguments,msg_from,msg)
+            self.handle_command(command_args,msg)
 
 
 
 
+    def handle_command(self, args, msg):
+        command = ""
 
+        if (len(args) == 1):
+            command = args[0]
+        else:
+            command = args[1]
 
+        def pong_handler():
+            self.send_msg("PONG " + msg)
+            print("pong")
 
+        def privmsg_handler():
+            msg_from = args[0].split("!")[0]
+            if(re.match("#", args[2])):
+                print(msg_from + "> " + args[2] + ": " + msg)
 
-  
+                if (re.match("!", msg)):
 
+                    def bot_time():
+                        self.send_msg("PRIVMSG " + args[2] + " :" + str(datetime.time(datetime.now())))
 
+                    def bot_date():
+                        self.send_msg("PRIVMSG " + args[2] + " :" + str(datetime.date(datetime.now())))
 
-    def testqwe(self):
-        if(self.test):
-            self.send_msg("PRIVMSG bot4 :test")
-            self.test = False
+                    bot_commands = {
+                        "!time": bot_time,
+                        "!date": bot_date
+                    }
+
+                    try:
+                        bot_commands[msg]()
+                    except KeyError:
+                        print("not a valid command")
+
+            else:
+                print(msg_from + ": " + msg)
+                self.send_msg("PRIVMSG " + msg_from + " :fuck off you stupid cunt")
+
+        print(command)
+        command_handlers = {
+            "PING": pong_handler,
+            "PRIVMSG": privmsg_handler
+        }
+
+        try:
+            command_handlers[command]()
+        except KeyError:
+            print("no handler for command/ reply number")
 
 
     def run(self):
         self.connect()
         while True:
-            self.testqwe()
             self.recieve()
             if(not self.buffer_empty()):
                 self.parse_buffer()
 
 
 
+parser = argparse.ArgumentParser(description = "A bot for the irc protocol")
+parser.add_argument('--server', help="server to connect to (default = '127.0.0.1')", default = "127.0.0.1")
+parser.add_argument('--nick', help="nickname to use on the server (default = 'Bot')", default = "Bot")
+parser.add_argument('--user', help="username to use on the server (default = 'Bot')", default = "Bot")
+parser.add_argument('--name', help="real name to use on the server (default = 'Bot')", default = "Bot")
+parser.add_argument('--channel', help="channel to connect to on the server (none by default)", default = "")
 
-bot = IRCBot("127.0.0.1", "bot2", "bot3", "bot4")
-bot.run() 
+print(" ___ ___  ___ ___      _ ")
+print("|_ _| _ \\/ __| _ ) ___| |_ ")
+print(" | ||   / (__| _ \\/ _ \\  _|")
+print("|___|_|_\\\\___|___/\\___/\\__|")
 
+args = parser.parse_args()
 
-
-
-
-
+bot = IRCBot(ip = args.server, user = args.user, name = args.name, nick = args.nick, channel = args.channel)
+bot.run()
