2364f24c8644e1bb5e08ceed4f3fb5ac0f4c6c4c
John <johngrossi1999@hotmail.com>
2019-11-29 14:31:17 +0000

    split stuff up into classes, sill in progress

--- Server/Server.py
+++ Server/Server.py
@@ -1,32 +1,99 @@
 import socket
 import select
-
-#all set up
-
-HEADER_LENGTH = 10
-
-IP = "127.0.0.1"
-PORT = 6667
-
-# Create a socket
-server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-#allow re-conect
-server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-
-# Bind, so server informs operating system that it's going to use given IP and port
-server_socket.bind((IP, PORT))
-
-# This makes server listen to new connections
-server_socket.listen()
+import errno
+import sys
+import re
+from datetime import datetime
+import time
+import argparse
 
 class Client(object):
+    commandList = {
+    "JOIN":___,
+    "LIST":___,
+    "MOTD":___,
+    "NAMES":__,
+    "NICK":___,
+    "PART":___,
+    "PING":___,
+    "PONG":___,
+    "PRIVMSG":_,
+    "QUIT":___,
+    "TOPIC":__,
+    "WALLOPS":_,
+    "WHO":____,
+    }
     #add people
     #remove people
     return
 class Server(object):
-    self.channels = {}
-    self.clients = {}
-    self.nicknames = {}
+    def __init__(self):
+        self.servername = 'IRC SERVER'
+        self.channels = {}
+        self.clients = {}
+
+    def start(self):
+        HEADER_LENGTH = 10
+
+        IP = "127.0.0.1"
+        PORT = 6667
+
+        # Create a socket
+        server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        #allow re-conect
+        server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+
+        # Bind, so server informs operating system that it's going to use given IP and port
+        server_socket.bind((IP, PORT))
+
+        # This makes server listen to new connections
+        server_socket.listen()
+        print("Listening for connections on "+IP+" ,Port: "+PORT+"")
+
+        # List of sockets for select.select()
+        sockets_list = [server_socket]
+
+        While True:
+            read_sockets, _, exception_sockets = select.select(sockets_list, [], sockets_list) #not 100% sure what this does
+            # Iterate over notified sockets
+            for notified_socket in read_sockets:
+
+                # If notified socket is a server socket - new connection, accept it
+                if notified_socket == server_socket:
+                    # Accept new connection
+                    client_socket, client_address = server_socket.accept()
+                    # Client should send his name right away, receive it
+                    user = receive_message(client_socket)
+                    sockets_list.append(client_socket)
+                    self.clients[client_socket] = user
+                    print("Accepted new connection")
+                #known connection
+                else:
+                    message = receive_message(notified_socket)
+                    user = clients[notified_socket]
+                    print("Received message from "+user["data"].decode("utf-8")+" : "+message["data"].decode("utf-8")+""")
+
+                    for client_socket in clients:
+                        client_socket.send(user['header'] + user['data'] + message['header'] + message['data'])
+
+    def receive_message(client_socket):
+        try:
+            # Receive our "header" containing message length, it's size is defined and constant
+            message_header = client_socket.recv(HEADER_LENGTH)
+
+            # If we received no data, client gracefully closed a connection, for example using socket.close() or socket.shutdown(socket.SHUT_RDWR)
+            if len(message_header) < 1:
+                return False
+
+            # Convert header to int value
+            message_length = int(message_header.decode('utf-8')
+
+            # Return an object of message header and message data
+            return {'header': message_header, 'data': client_socket.recv(message_length)}
+
+        except:
+            # client closed connection
+            return False
     return
 
 class Channel(object):
@@ -38,7 +105,9 @@ class Channel(object):
         #remove people
     return
 
-
+def main(arg):
+    server = Server()
+    return
 
 
 
